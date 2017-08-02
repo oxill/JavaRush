f7a2f48d54866a6d076d9ac88e7600da7a962f0f
open.ru\okhanin_id <ilya.okhanin@open.ru>
2017-08-02 18:40:17 +0400

    JavaCore\src\com\javarush\task\task19\task1905

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1905/Solution.java
@@ -0,0 +1,106 @@
+package com.javarush.task.task19.task1905;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Закрепляем адаптер
+*/
+
+public class Solution {
+    public static Map<String,String> countries = new HashMap<String,String>();
+
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
+    }
+
+    public static void main(String[] args) {
+        RowItem rowItem = new DataAdapter(new Customer() {
+            @Override
+            public String getCompanyName() {
+                return "JavaRush Ltd.";
+            }
+
+            @Override
+            public String getCountryName() {
+                return "Ukraine";
+            }
+        }, new Contact() {
+            @Override
+            public String getName() {
+                return "Ivanov, Ivan";
+            }
+
+            @Override
+            public String getPhoneNumber() {
+                return "+38(050)123-45-67";
+            }
+        });
+
+        System.out.println(rowItem.getCompany());
+        System.out.println(rowItem.getContactFirstName());
+        System.out.println(rowItem.getContactLastName());
+        System.out.println(rowItem.getCountryCode());
+        System.out.println(rowItem.getDialString());
+    }
+
+    public static class DataAdapter implements RowItem {
+        private Customer customer;
+        private Contact contact;
+
+        public DataAdapter(Customer customer, Contact contact) {
+            this.customer = customer;
+            this.contact = contact;
+        }
+
+        @Override
+        public String getCountryCode() {
+            for (Map.Entry<String, String> entry :
+                    countries.entrySet()) {
+                if (entry.getValue().equals(customer.getCountryName()))
+                    return entry.getKey();
+            }
+            return null;
+        }
+
+        @Override
+        public String getCompany() {
+            return customer.getCompanyName();
+        }
+
+        @Override
+        public String getContactFirstName() {
+            return contact.getName().split(",")[1].trim();
+        }
+
+        @Override
+        public String getContactLastName() {
+            return contact.getName().split(",")[0].trim();
+        }
+
+        @Override
+        public String getDialString() {
+            return "callto://" + contact.getPhoneNumber().replaceAll("\\(","").replaceAll("\\)", "").replaceAll("-", "");
+        }
+    }
+
+    public static interface RowItem {
+        String getCountryCode();        //example UA
+        String getCompany();            //example JavaRush Ltd.
+        String getContactFirstName();   //example Ivan
+        String getContactLastName();    //example Ivanov
+        String getDialString();         //example callto://+380501234567
+    }
+
+    public static interface Customer {
+        String getCompanyName();        //example JavaRush Ltd.
+        String getCountryName();        //example Ukraine
+    }
+
+    public static interface Contact {
+        String getName();               //example Ivanov, Ivan
+        String getPhoneNumber();        //example +38(050)123-45-67
+    }
+}
