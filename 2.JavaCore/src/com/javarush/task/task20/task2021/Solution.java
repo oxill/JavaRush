0b99d9a22476a750d2d3201500e545d0acaabde1
open.ru\okhanin_id <ilya.okhanin@open.ru>
2017-08-14 17:23:29 +0400

    JavaCore\src\com\javarush\task\task20\task2021

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2021/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task20.task2021;
+
+import java.io.*;
+
+/* 
+Сериализация под запретом
+*/
+public class Solution implements Serializable {
+    public static class SubSolution extends Solution {
+
+        public void writeObject(ObjectOutput out) throws IOException {
+            throw new NotSerializableException();
+        }
+
+        public void readObject(ObjectInput in) throws IOException, ClassNotFoundException {
+            throw new NotSerializableException();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
