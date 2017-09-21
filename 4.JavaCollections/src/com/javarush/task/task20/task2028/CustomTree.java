7c0565a84d956faf9442318388d6586d35ca0466
open.ru\okhanin_id <ilya.okhanin@open.ru>
2017-09-21 17:46:36 +0400

    JavaCollections\src\com\javarush\task\task20\task2028

---
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -0,0 +1,107 @@
+package com.javarush.task.task20.task2028;
+
+import java.io.Serializable;
+import java.util.AbstractList;
+import java.util.Collection;
+import java.util.List;
+
+/* 
+Построй дерево(1)
+*/
+public class CustomTree extends AbstractList<String> implements Cloneable, Serializable{
+    public static void main(String[] args) {
+        List<String> list = new CustomTree();
+        for (int i = 1; i < 16; i++) {
+            list.add(String.valueOf(i));
+        }
+        //System.out.println("Expected 3, actual is " + ((CustomTree) list).getParent("8"));
+        list.remove("5");
+        //System.out.println("Expected null, actual is " + ((CustomTree) list).getParent("11"));
+    }
+
+    static class Entry<T> implements Serializable {
+        String elementName;
+        int lineNumber = 0;
+        boolean availableToAddLeftChildren, availableToAddRightChildren;
+        Entry<T> parent, leftChild, rightChild;
+        Entry<String> root;
+
+        public Entry(String elementName) {
+            this.elementName = elementName;
+            availableToAddLeftChildren = true;
+            availableToAddRightChildren = true;
+        }
+
+        void checkChildren() {
+            if (leftChild != null)
+                availableToAddLeftChildren = false;
+            if (rightChild != null)
+                availableToAddRightChildren = false;
+        }
+
+        boolean isAvailableToAddChildren() {
+            return availableToAddLeftChildren || availableToAddRightChildren;
+        }
+
+        int size() {
+            return lineNumber;
+        }
+
+        void add(String name) {
+            if (root == null) {
+                root = new Entry<>(name);
+            }
+            else {
+                checkChildren();
+                if (availableToAddLeftChildren)
+                    leftChild = new Entry<>(name);
+                else if (availableToAddRightChildren)
+                    rightChild = new Entry<>(name);
+            }
+        }
+
+        void remove(String s) {
+
+        }
+    }
+
+    @Override
+    public String get(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String set(int index, String element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void add(int index, String element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String remove(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<String> subList(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void removeRange(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends String> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
